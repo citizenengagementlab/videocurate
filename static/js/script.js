4530981cf4c73e3e094b22e4e61c82994c83aded
Benjamin Wilkins <becowilk@gmail.com>
2011-10-27 13:11:25 -0700

    Resize embed to 100%;

--- static/js/script.js
+++ static/js/script.js
@@ -1,10 +1,11 @@
 /* Author:
-
+   Benjamin Wilkins
+   Josh Levinger
+   Citizen Engagement Laboratory
 */
 
-
-
-
-
-
-
+$(document).ready(
+	function() {
+		$("#embed").children().attr("width", "100%")
+	}
+);
