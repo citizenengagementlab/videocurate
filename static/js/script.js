eb2fa19a9f368dae5c01e2ae25730740623cb28d
Benjamin Wilkins <becowilk@gmail.com>
2011-10-27 13:43:59 -0700

    add video form, removed background color divs, hid banner on add page.

--- static/js/script.js
+++ static/js/script.js
@@ -6,6 +6,12 @@
 
 $(document).ready(
 	function() {
+		//set embed width to 100%
 		$("#embed").children().attr("width", "100%")
+		//hide banner section on add page
+		if ( window.location.href.match(/\/add/) ) {
+			$("#banner").hide();
+		}
 	}
 );
+if (true) {};
