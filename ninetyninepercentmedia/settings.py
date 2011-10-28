c9014d6cc2b68a70a09836ee7c407ed1e34e4c75
Josh Levinger <josh@levinger.net>
2011-10-27 18:33:26 -0700

    add debug toolbar

--- ninetyninepercentmedia/settings.py
+++ ninetyninepercentmedia/settings.py
@@ -79,6 +79,7 @@ MIDDLEWARE_CLASSES = (
     'secretballot.middleware.SecretBallotIpMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
+    'debug_toolbar.middleware.DebugToolbarMiddleware',
 )
 
 ROOT_URLCONF = 'ninetyninepercentmedia.urls'
