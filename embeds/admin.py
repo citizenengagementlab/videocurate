e08e5cbcd0ccca852436fb6a14cc4da3fb47dbcb
Josh Levinger <josh@levinger.net>
2011-11-17 17:27:14 -0800

    embed admin

--- embeds/admin.py
+++ embeds/admin.py
@@ -1,4 +1,8 @@
 from django.contrib import admin
 from embeds.models import *
 
-admin.site.register(SavedEmbed)
+class EmbedAdmin(admin.ModelAdmin):
+    list_display = ('url','type','last_updated')
+    ordering = ('-last_updated',)
+
+admin.site.register(SavedEmbed,EmbedAdmin)
