bf554eb14f665eb10465318e36ff83561d4a929a
Josh Levinger <josh.l@engagementlab.org>
2011-11-30 17:42:06 -0800

    add upload stats by day

--- mediacurate/management/commands/print_stats.py
+++ mediacurate/management/commands/print_stats.py
@@ -6,6 +6,8 @@ from mediacurate.models import Media
 from secretballot.models import Vote
 from django.contrib.comments.models import Comment
 
+import qsstats #http://pypi.python.org/pypi/django-qsstats-magic
+import datetime
 
 class Command(BaseCommand):
     def handle(self, *args, **options):
@@ -21,3 +23,12 @@ class Command(BaseCommand):
         comments = Comment.objects.filter(content_type=media_type)
         comment_ips = comments.values('ip_address').distinct()
         print "%d comments from %d IPs" % (comments.count(), comment_ips.count())
+        
+        
+        qss = qsstats.QuerySetStats(qs=Media.objects.all(),date_field='date_added')
+        end = datetime.date.today() + datetime.timedelta(days=1) #fwd a day because the end date isn't inclusive
+        start = datetime.date(2011,10,30) #site launch date
+        results = qss.time_series(start,end,"days")
+        print "\nUploads by date"
+        for t in results:
+            print "%s,%s" % (t[0].strftime('%x'),t[1])
